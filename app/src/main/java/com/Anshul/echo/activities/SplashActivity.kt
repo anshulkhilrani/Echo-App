fcdb9385db526e4be3ef6c2db0612ca77e296c4d
Anshul Khilrani <39955700+FortMinor18@users.noreply.github.com>
2020-10-04 20:31:56 +0530

    Update SplashActivity.kt

--- app/src/main/java/com/Anshul/echo/activities/SplashActivity.kt
+++ app/src/main/java/com/Anshul/echo/activities/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.Aryan.echo.activities
+package com.Anshul.echo.activities
 
 import android.Manifest
 import android.content.Context
@@ -9,7 +9,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.ActivityCompat
 import android.widget.Toast
-import com.Aryan.echo.R
+import com.Anshul.echo.R
 
 class SplashActivity : AppCompatActivity() {
 
