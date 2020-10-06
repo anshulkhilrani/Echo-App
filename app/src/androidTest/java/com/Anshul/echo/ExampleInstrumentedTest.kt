9c69230701e12e01a787d639182b93aba0ce59f7
Anshul Khilrani <39955700+FortMinor18@users.noreply.github.com>
2020-10-06 10:13:11 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/Anshul/echo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/Anshul/echo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.shivamkapila.echo", appContext.packageName)
+        assertEquals("com.anshulkhilrani.echo", appContext.packageName)
     }
 }
