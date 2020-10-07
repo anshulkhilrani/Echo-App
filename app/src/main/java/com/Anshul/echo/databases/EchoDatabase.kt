1d98573084425df3256203a85e041b9dd134efac
Anshul Khilrani <39955700+FortMinor18@users.noreply.github.com>
2020-10-08 04:08:37 +0530

    Update EchoDatabase.kt

--- app/src/main/java/com/Anshul/echo/databases/EchoDatabase.kt
+++ app/src/main/java/com/Anshul/echo/databases/EchoDatabase.kt
@@ -1,10 +1,10 @@
-package com.Aryan.echo.databases
+package com.Anshul.echo.databases
 
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import com.Aryan.echo.Songs
+import com.Anshul.echo.Songs
 
 class EchoDatabase : SQLiteOpenHelper {
 
